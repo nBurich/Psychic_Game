6b7932b2651849fdb86272f83dbcf30671ceec3c
nBurich <n.burich@hotmail.com>
2018-12-13 23:35:33 -0500

    Psychic_Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 var computerChoices= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]; 
 var wins = 0;
 var losses = 0;
-var numGuesses = 10;
+var guesses = 10;
 var userGuess= [];
 
 
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text")
-var numGuessesText = document.getElementById("numGuesses-text")
+var guessesText = document.getElementById("guesses-text")
 var userGuessText = document.getElementById("userGuess-text")
 
 
@@ -16,25 +16,16 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     var computerAns = computerChoices[Math.floor(Math.random()* computerChoices.length)];
 
-    if ((userGuess === computerChoices) {
+    if (userGuess === computerChoices) {
         wins++;
-        numGuesses = 10;
-        guessChoices = [];
-    }
-        if else (userGuess != computerGuess) {
-         numGuesses --;
-         guessChoices.push(userGuess);
-    }
+        guesses = 10;
+    }   
     else {
-        numGuesses = 10;
+        guesses= guesses - 1;
         losses ++;
-        userGuess = [];
-    };
-
-
-document.write() = "Wins:"+ wins;
-document.write() = "Losses:" + losses;
-document.write() = "Guesses Left:" + guesses ;
-document.write() = "Your Guesses so far:"+  event.key  ;
-
-};
+    }
+    winsText.textContent = "Wins:"+ wins;
+    lossesText.textContent = "Losses:" + losses;
+    guessesText.textContent = "Guesses Left:" + guesses;
+    userGuessText.textContent = "Your Guesses so far: "+  userGuess;
+}
